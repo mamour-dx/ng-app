a5ee49f2b1f900ff9076c49860d36efd5a8d98b9
Mamour Dieng <mamour.dieng@socium.link>
2025-05-24 14:54:42 +0000

    refactor TodosService to use HttpClient for fetching todos from API

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,22 +1,16 @@
-import { Injectable } from '@angular/core';
-import { Todo } from "../models/todo.type"
+import { Todo } from './../models/todo.type';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
-  todoItems : Array<Todo>  = [{
-    title: 'groceries', 
-    id: 0,
-    userId: 1,
-    completed: false
-  }, {
-    title: 'gym',
-    id: 1,
-    userId: 1,
-    completed: true
+  http = inject(HttpClient);
+  getTodosFromApi() {
+    const url = "https://jsonplaceholder.typicode.com/todos"
+    return this.http.get<Array<Todo>>(url)
   }
-]
   constructor() { }
 }
        
