61add162e972b7a0d8d05b7f80854738be490e7d
Mamour Dieng <mamour.dieng@socium.link>
2025-05-24 14:55:01 +0000

    implement todos list display and fetch todos from API

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,5 +1,7 @@
-import { Component, inject} from '@angular/core';
-import { TodosService } from '../services/todos.service'
+import { Todo } from './../models/todo.type';
+import { TodosService } from './../services/todos.service';
+import { Component, inject, OnInit, signal} from '@angular/core';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
@@ -7,6 +9,23 @@ import { TodosService } from '../services/todos.service'
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit {
   todoService = inject(TodosService);
+  todoItems = signal<Array<Todo>>([])
+  
+
+  
+  ngOnInit(): void {
+    this.todoService
+    .getTodosFromApi()
+    .pipe (
+      catchError((err) => {
+        throw err
+      })
+    )
+    // this is critical
+    .subscribe((todos: Todo[]) => {
+      this.todoItems.set(todos);
+    })
+  }
 }
