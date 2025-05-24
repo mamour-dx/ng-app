f5360d750f12ef4a1d59817a38ee72daa0a6a1b2
Mamour Dieng <mamour.dieng@socium.link>
2025-05-24 14:53:40 +0000

    inject TodosService into HeaderComponent

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router'
+import { TodosService } from '../../services/todos.service';
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [RouterLink],
   templateUrl: `./header.component.html`,
-  styleUrl: `./header.component.scss`
+  styleUrl: `./header.component.scss`,
+  providers: [TodosService]
+
 })
 export class HeaderComponent {
 
